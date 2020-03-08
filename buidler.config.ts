5ce8b413c06185eb21059f2aa544f604b07ea7a7
Mick de Graaf <mick@mickdegraaf.nl>
2020-03-08 12:33:09 +0100

    Boilerplate

---
+++ buidler.config.ts
@@ -0,0 +1,43 @@
+import { BuidlerConfig, usePlugin, task } from "@nomiclabs/buidler/config";
+
+usePlugin("@nomiclabs/buidler-waffle");
+usePlugin("@nomiclabs/buidler-etherscan");
+usePlugin("buidler-typechain");
+usePlugin("solidity-coverage");
+
+const INFURA_API_KEY = "";
+const RINKEBY_PRIVATE_KEY = "";
+const ETHERSCAN_API_KEY = "";
+
+interface ExtendedBuidlerConfig extends BuidlerConfig {
+  [x:string]: any
+}
+
+const config: ExtendedBuidlerConfig = {
+  defaultNetwork: "buidlerevm",
+  solc: {
+    version: "0.6.2"
+  },
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [RINKEBY_PRIVATE_KEY]
+    },
+    coverage: {
+      url: 'http://127.0.0.1:8555' // Coverage launches its own ganache-cli client
+    }
+  },
+  etherscan: {
+    // The url for the Etherscan API you want to use.
+    url: "https://api-rinkeby.etherscan.io/api",
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers"
+  }
+};
+
+export default config;
