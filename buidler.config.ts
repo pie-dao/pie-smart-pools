47422a33c1e7d916b2a2cb3f3fd9a449a342eb53
Mick de Graaf <mick@mickdegraaf.nl>
2020-03-10 23:37:18 +0100

    Buidler deploy task

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,13 +1,18 @@
+require("dotenv").config();
 import { BuidlerConfig, usePlugin, task } from "@nomiclabs/buidler/config";
+import { PBasicSmartPoolFactory } from "./typechain/PBasicSmartPoolFactory";
+import { utils } from "ethers";
 
 usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("@nomiclabs/buidler-etherscan");
 usePlugin("buidler-typechain");
 usePlugin("solidity-coverage");
 
-const INFURA_API_KEY = "";
-const RINKEBY_PRIVATE_KEY = "";
-const ETHERSCAN_API_KEY = "";
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+const KOVAN_PRIVATE_KEY = process.env.KOVAN_PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
+console.log(KOVAN_PRIVATE_KEY);
 
 interface ExtendedBuidlerConfig extends BuidlerConfig {
   [x:string]: any
@@ -19,9 +24,9 @@ const config: ExtendedBuidlerConfig = {
     version: "0.6.2"
   },
   networks: {
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [RINKEBY_PRIVATE_KEY]
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [KOVAN_PRIVATE_KEY]
     },
     coverage: {
       url: 'http://127.0.0.1:8555' // Coverage launches its own ganache-cli client
@@ -39,5 +44,18 @@ const config: ExtendedBuidlerConfig = {
     target: "ethers"
   }
 };
+
+task("deploy-pie-smart-pool", "deploys a pie smart pool")
+  .addParam("pool", "Balancer pool address (should have tokens binded)")
+  .addParam("name", "Name of the token")
+  .addParam("symbol", "Symbol of the token")
+  .addParam("initialSupply", "Initial supply of the token")
+  .setAction(async(taskArgs, { ethers }) => {
+    const signers = await ethers.getSigners();
+    const factory = new PBasicSmartPoolFactory(signers[0]);
+    const smartpool = await factory.deploy(taskArgs.pool, taskArgs.name, taskArgs.symbol, utils.parseEther(taskArgs.initialSupply));
+
+    console.log(`PBasicSmartPool deployed at: ${smartpool.address}`);
+})
 
 export default config;
