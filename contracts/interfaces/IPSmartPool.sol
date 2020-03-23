338894c210e42be5e01b7cad1eb0ae88408a2d90
Mick de Graaf <mick@mickdegraaf.nl>
2020-03-23 15:57:15 +0100

    Inherit IERC20 interface in IPSmartPool

--- contracts/interfaces/IPSmartPool.sol
+++ contracts/interfaces/IPSmartPool.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.6.4;
 
-interface IPSmartPool {
+import "./IERC20.sol";
+interface IPSmartPool is IERC20 {
     function joinPool(uint256 _amount) external;
     function exitPool(uint256 _amount) external;
     function getController() external view returns(address);
