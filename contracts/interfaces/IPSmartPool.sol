a2e1a5c7356521d9a5305af7389b65cc859895ec
Mick de Graaf <mick@mickdegraaf.nl>
2020-03-23 15:51:09 +0100

    New interfaces

--- contracts/interfaces/IPSmartPool.sol
+++ contracts/interfaces/IPSmartPool.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.6.4;
 
-
-// Based on the liquidity bootstrapping pool by Balancer
 interface IPSmartPool {
     function joinPool(uint256 _amount) external;
     function exitPool(uint256 _amount) external;
+    function getController() external view returns(address);
+    function getTokens() external view returns(address[] memory);
+    function calcTokensForAmount(uint256 _amount) external view  returns(address[] memory tokens, uint256[] memory amounts);
 }
