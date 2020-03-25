6bea7a0c50fa9558ba0b3eb89e20c59088d6eade
Mick de Graaf <mick@mickdegraaf.nl>
2020-03-25 14:17:17 +0100

    Kyber recipe implementation

--- contracts/recipes/PUniswapKyberPoolRecipe.sol
+++ contracts/recipes/PUniswapKyberPoolRecipe.sol
@@ -5,44 +5,35 @@ import "../interfaces/IKyberNetwork.sol";
 
 contract PUniswapKyberPoolRecipe is PUniswapPoolRecipe {
 
+    // TODO use diamond storage
     mapping(address => bool) public swapOnKyber;
     IKyberNetwork public kyber;
+    address public feeReceiver;
+
+    address public constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     
-    constructor(address _pool, address _uniswapFactory, address _kyber, address[] memory _swapOnKyber) PUniswapPoolRecipe(_pool, _uniswapFactory) public {
+    constructor(address _pool, address _uniswapFactory, address _kyber, address[] memory _swapOnKyber, address _feeReciever) PUniswapPoolRecipe(_pool, _uniswapFactory) public {
         kyber = IKyberNetwork(_kyber);
+        feeReceiver = _feeReciever;
 
         for(uint256 i = 0; i < _swapOnKyber.length; i ++) {
             swapOnKyber[_swapOnKyber[i]] = true;
         }
     }
 
-
-    // TODO implement getting prices from kyber
-
-    // function getEthToTokenOutputPrice(uint256 _tokens_bought) external view override returns (uint256 eth_sold) {
-    //     if(!swapOnKyber[_token]) {
-    //         return super.getEthToTokenOutputPrice(_tokens_bought);
-    //     }
-
-    //     // TODO get kyber price
-    // }
-
-
-    // function getTokenToEthInputPrice(uint256 _tokens_sold) external view override returns (uint256 eth_bought) {
-    //     if(!swapOnKyber[_token]) {
-    //         return super.getTokenToEthInputPrice(_tokens_sold);
-    //     }
-
-    //     // TODO get kyber price
-    // }
-
+    // TODO set swap on kyber
 
     function _ethToToken(address _token, uint256 _tokens_bought) internal override returns (uint256) {
         if(!swapOnKyber[_token]) {
             return super._ethToToken(_token, _tokens_bought);
         }
 
-        // TODO swap on kyber
+        uint256 ethBefore = address(this).balance;
+        kyber.trade(ETH, address(this).balance, _token, address(this), _tokens_bought, 1, feeReceiver);
+        uint256 ethAfter = address(this).balance;
+
+        // return amount of ETH spend
+        return ethBefore - ethAfter;
     }
 
     function _tokenToEth(IERC20 _token, uint256 _tokens_sold, address _recipient) internal override returns (uint256 eth_bought) {
@@ -51,6 +42,12 @@ contract PUniswapKyberPoolRecipe is PUniswapPoolRecipe {
         }
 
         // TODO swap on kyber
+        uint256 ethBefore = address(this).balance;
+        kyber.trade(address(_token), _tokens_sold, ETH, address(this), uint256(-1), 1, feeReceiver);
+        uint256 ethAfter = address(this).balance;
+
+        // return amount of ETH received
+        return ethAfter - ethBefore;
     }
 
 }
