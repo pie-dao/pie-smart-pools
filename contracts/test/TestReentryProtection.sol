43ea6bfbadb5fb079f74fb35947d4870a5e4d5a0
Mick de Graaf <mick@mickdegraaf.nl>
2020-04-02 11:53:51 +0200

    Reentry protection test

---
+++ contracts/test/TestReentryProtection.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.6.4;
+
+import "../ReentryProtection.sol";
+
+contract TestReentryProtection is ReentryProtection {
+
+    // This should fail
+    function test() external noReentry {
+        reenter();
+    }
+
+    function reenter() public noReentry {
+        // Do nothing
+    }
+
+}
---
+++ test/reentryProtection.ts
@@ -0,0 +1,30 @@
+// This way of importing is a bit funky. We should fix this in the Mock Contracts package
+import { ethers } from "@nomiclabs/buidler";
+import { Signer, Wallet, utils, constants } from "ethers";
+import chai from "chai";
+import { deployContract, solidity } from "ethereum-waffle";
+import { TestReentryProtection } from "../typechain/TestReentryProtection";
+import TestReentryArtifact from "../artifacts/TestReentryProtection.json";
+
+chai.use(solidity);
+const { expect } = chai;
+
+describe("ReentryProtection", function() {
+    this.timeout(300000);
+    let signers: Signer[];
+    let account: string;
+    let testContract: TestReentryProtection
+
+    beforeEach(async() => {
+        signers = await ethers.signers();
+        account = await signers[0].getAddress();
+
+        // smartpool = await deployContract(signers[0] as Wallet, PBasicSmartPoolArtifact, [], {gasLimit: 8000000}) as PBasicSmartPool
+        testContract = await deployContract(signers[0] as Wallet, TestReentryArtifact, []) as TestReentryProtection;
+    });
+
+    it.only("Should prevent reentry", async() => {
+        await expect(testContract.test()).to.be.reverted;
+    })
+
+});
