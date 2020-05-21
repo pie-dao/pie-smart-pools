bd2bcad06e77d6059254b11cce2d06af9b315ec2
Mick de Graaf <mick@mickdegraaf.nl>
2020-05-21 12:17:18 +0200

    Mainnet test (#22)
    * Mainnet test
    
    * Test fixes
    
    * checking balances after exit with loss
    
    * Spacing fixes
    
    * Remove TODO
    
    Co-authored-by: Dan Matthews <dan@cryptobuilders.io>

--- contracts/smart-pools/PBasicSmartPool.sol
+++ contracts/smart-pools/PBasicSmartPool.sol
@@ -135,7 +135,7 @@ contract PBasicSmartPool is IPSmartPool, PCToken, ReentryProtection {
         lpbs().bPool.setSwapFee(_swapFee);
     }
 
-    /** 
+    /**
         @notice Mints pool shares in exchange for underlying assets
         @param _amount Amount of pool shares to mint
     */
