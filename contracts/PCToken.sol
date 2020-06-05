2b3778f1b5939c140bc857ccb8952ab8877282f7
Travis Mathis <travis@marketprotocol.io>
2020-06-05 11:04:49 -0400

    runs prettier against codebase (#38)
    * runs prettier against codebase
    
    * runs prettier against all contracts

--- contracts/PCToken.sol
+++ contracts/PCToken.sol
@@ -17,108 +17,118 @@ import "./PCTokenStorage.sol";
 import "./interfaces/IERC20.sol";
 import "./Math.sol";
 
+
 // Highly opinionated token implementation
 // Based on the balancer Implementation
 
-
 contract PCToken is IERC20, PCTokenStorage {
+  using Math for uint256;
 
-    using Math for uint256;
+  event Approval(address indexed _src, address indexed _dst, uint256 _amount);
+  event Transfer(address indexed _src, address indexed _dst, uint256 _amount);
 
-    event Approval(address indexed _src, address indexed _dst, uint _amount);
-    event Transfer(address indexed _src, address indexed _dst, uint _amount);
+  uint8 public constant decimals = 18;
 
-    uint8 public constant decimals = 18;
+  function _mint(uint256 _amount) internal {
+    pts storage s = lpts();
+    s.balance[address(this)] = s.balance[address(this)].badd(_amount);
+    s.totalSupply = s.totalSupply.badd(_amount);
+    emit Transfer(address(0), address(this), _amount);
+  }
 
-    function _mint(uint _amount) internal {
-        pts storage s = lpts();
-        s.balance[address(this)] = s.balance[address(this)].badd(_amount);
-        s.totalSupply = s.totalSupply.badd(_amount);
-        emit Transfer(address(0), address(this), _amount);
+  function _burn(uint256 _amount) internal {
+    pts storage s = lpts();
+    require(s.balance[address(this)] >= _amount, "ERR_INSUFFICIENT_BAL");
+    s.balance[address(this)] = s.balance[address(this)].bsub(_amount);
+    s.totalSupply = s.totalSupply.bsub(_amount);
+    emit Transfer(address(this), address(0), _amount);
+  }
+
+  function _move(
+    address _src,
+    address _dst,
+    uint256 _amount
+  ) internal {
+    pts storage s = lpts();
+    require(s.balance[_src] >= _amount, "ERR_INSUFFICIENT_BAL");
+    s.balance[_src] = s.balance[_src].bsub(_amount);
+    s.balance[_dst] = s.balance[_dst].badd(_amount);
+    emit Transfer(_src, _dst, _amount);
+  }
+
+  function _push(address _to, uint256 _amount) internal {
+    _move(address(this), _to, _amount);
+  }
+
+  function _pull(address _from, uint256 _amount) internal {
+    _move(_from, address(this), _amount);
+  }
+
+  function allowance(address _src, address _dst) external override view returns (uint256) {
+    return lpts().allowance[_src][_dst];
+  }
+
+  function balanceOf(address _whom) external override view returns (uint256) {
+    return lpts().balance[_whom];
+  }
+
+  function totalSupply() public override view returns (uint256) {
+    return lpts().totalSupply;
+  }
+
+  function name() external view returns (string memory) {
+    return lpts().name;
+  }
+
+  function symbol() external view returns (string memory) {
+    return lpts().symbol;
+  }
+
+  function approve(address _dst, uint256 _amount) external override returns (bool) {
+    lpts().allowance[msg.sender][_dst] = _amount;
+    emit Approval(msg.sender, _dst, _amount);
+    return true;
+  }
+
+  function increaseApproval(address _dst, uint256 _amount) external returns (bool) {
+    pts storage s = lpts();
+    s.allowance[msg.sender][_dst] = s.allowance[msg.sender][_dst].badd(_amount);
+    emit Approval(msg.sender, _dst, s.allowance[msg.sender][_dst]);
+    return true;
+  }
+
+  function decreaseApproval(address _dst, uint256 _amount) external returns (bool) {
+    pts storage s = lpts();
+    uint256 oldValue = s.allowance[msg.sender][_dst];
+    if (_amount > oldValue) {
+      s.allowance[msg.sender][_dst] = 0;
+    } else {
+      s.allowance[msg.sender][_dst] = oldValue.bsub(_amount);
     }
+    emit Approval(msg.sender, _dst, s.allowance[msg.sender][_dst]);
+    return true;
+  }
 
-    function _burn(uint _amount) internal {
-        pts storage s = lpts();
-        require(s.balance[address(this)] >= _amount, "ERR_INSUFFICIENT_BAL");
-        s.balance[address(this)] = s.balance[address(this)].bsub(_amount);
-        s.totalSupply = s.totalSupply.bsub(_amount);
-        emit Transfer(address(this), address(0), _amount);
-    }
+  function transfer(address _dst, uint256 _amount) external override returns (bool) {
+    _move(msg.sender, _dst, _amount);
+    return true;
+  }
 
-    function _move(address _src, address _dst, uint _amount) internal {
-        pts storage s = lpts();
-        require(s.balance[_src] >= _amount, "ERR_INSUFFICIENT_BAL");
-        s.balance[_src] = s.balance[_src].bsub(_amount);
-        s.balance[_dst] = s.balance[_dst].badd(_amount);
-        emit Transfer(_src, _dst, _amount);
+  function transferFrom(
+    address _src,
+    address _dst,
+    uint256 _amount
+  ) external override returns (bool) {
+    pts storage s = lpts();
+    require(
+      msg.sender == _src || _amount <= s.allowance[_src][msg.sender],
+      "ERR_PCTOKEN_BAD_CALLER"
+    );
+    _move(_src, _dst, _amount);
+    if (msg.sender != _src && s.allowance[_src][msg.sender] != uint256(-1)) {
+      s.allowance[_src][msg.sender] = s.allowance[_src][msg.sender].bsub(_amount);
+      emit Approval(msg.sender, _dst, s.allowance[_src][msg.sender]);
     }
-
-    function _push(address _to, uint _amount) internal {
-        _move(address(this), _to, _amount);
-    }
-
-    function _pull(address _from, uint _amount) internal {
-        _move(_from, address(this), _amount);
-    }
-
-    function allowance(address _src, address _dst) external view override returns (uint) {
-        return lpts().allowance[_src][_dst];
-    }
-
-    function balanceOf(address _whom) external view override returns (uint) {
-        return lpts().balance[_whom];
-    }
-
-    function totalSupply() public view override returns (uint) {
-        return lpts().totalSupply;
-    }
-
-    function name() external view returns (string memory) {
-        return lpts().name;
-    }
-
-    function symbol() external view returns (string memory) {
-        return lpts().symbol;
-    }
-
-    function approve(address _dst, uint _amount) external override returns (bool) {
-        lpts().allowance[msg.sender][_dst] = _amount;
-        emit Approval(msg.sender, _dst, _amount);
-        return true;
-    }
-
-    function increaseApproval(address _dst, uint _amount) external returns (bool) {
-        pts storage s = lpts();
-        s.allowance[msg.sender][_dst] = s.allowance[msg.sender][_dst].badd(_amount);
-        emit Approval(msg.sender, _dst, s.allowance[msg.sender][_dst]);
-        return true;
-    }
-
-    function decreaseApproval(address _dst, uint _amount) external returns (bool) {
-        pts storage s = lpts();
-        uint oldValue = s.allowance[msg.sender][_dst];
-        if (_amount > oldValue) {
-            s.allowance[msg.sender][_dst] = 0;
-        } else {
-            s.allowance[msg.sender][_dst] = oldValue.bsub(_amount);
-        }
-        emit Approval(msg.sender, _dst, s.allowance[msg.sender][_dst]);
-        return true;
-    }
-
-    function transfer(address _dst, uint _amount) external override returns (bool) {
-        _move(msg.sender, _dst, _amount);
-        return true;
-    }
-
-    function transferFrom(address _src, address _dst, uint _amount) external override returns (bool) {
-        pts storage s = lpts();
-        require(msg.sender == _src || _amount <= s.allowance[_src][msg.sender], "ERR_PCTOKEN_BAD_CALLER");
-        _move(_src, _dst, _amount);
-        if (msg.sender != _src && s.allowance[_src][msg.sender] != uint256(-1)) {
-            s.allowance[_src][msg.sender] = s.allowance[_src][msg.sender].bsub(_amount);
-            emit Approval(msg.sender, _dst, s.allowance[_src][msg.sender]);
-        }
-        return true;
-    }
+    return true;
+  }
 }
--- contracts/PCTokenStorage.sol
+++ contracts/PCTokenStorage.sol
@@ -1,25 +1,24 @@
 pragma solidity 0.6.4;
 
+
 contract PCTokenStorage {
+  bytes32 public constant ptSlot = keccak256("PCToken.storage.location");
+  struct pts {
+    string name;
+    string symbol;
+    uint256 totalSupply;
+    mapping(address => uint256) balance;
+    mapping(address => mapping(address => uint256)) allowance;
+  }
 
-    bytes32 constant public ptSlot = keccak256("PCToken.storage.location");
-    struct pts {
-        string name;
-        string symbol;
-        uint256 totalSupply;
-        mapping(address => uint256) balance;
-        mapping(address => mapping(address=>uint256)) allowance;
-    }
-
-    /**
+  /**
         @notice Load pool token storage
         @return s Storage pointer to the pool token struct
     */
-    function lpts() internal pure returns (pts storage s) {
-        bytes32 loc = ptSlot;
-        assembly {
-            s_slot := loc
-        }
+  function lpts() internal pure returns (pts storage s) {
+    bytes32 loc = ptSlot;
+    assembly {
+      s_slot := loc
     }
-
+  }
 }
