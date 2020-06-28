976c88067b1f381ace864431073187cfab0bb7c1
Mick de Graaf <mick@mickdegraaf.nl>
2020-06-28 11:47:47 +0200

    Use lib for weight changing functions

--- contracts/smart-pools/PAdjustableSmartPool.sol
+++ contracts/smart-pools/PAdjustableSmartPool.sol
@@ -22,72 +22,11 @@ contract PAdjustableSmartPool is PCappedSmartPool {
     uint256 _startBlock,
     uint256 _endBlock
   ) external noReentry onlyController {
-    pbs storage s = lpbs();
-    PAStorage.StorageStruct storage ws = PAStorage.load();
-
-    uint256 weightsSum = 0;
-    address[] memory tokens = s.bPool.getCurrentTokens();
-    // Check that endWeights are valid now to avoid reverting in a future pokeWeights call
-    for (uint256 i = 0; i < tokens.length; i++) {
-      require(_newWeights[i] <= constants.MAX_WEIGHT, "ERR_WEIGHT_ABOVE_MAX");
-      require(_newWeights[i] >= constants.MIN_WEIGHT, "ERR_WEIGHT_BELOW_MIN");
-      weightsSum = weightsSum.badd(_newWeights[i]);
-    }
-    require(weightsSum <= constants.MAX_TOTAL_WEIGHT, "ERR_MAX_TOTAL_WEIGHT");
-
-    if (block.number > _startBlock) {
-      // This means the weight update should start ASAP
-      ws.startBlock = block.number;
-    } else {
-        ws.startBlock = _startBlock;
-    }
-    ws.endBlock = _endBlock;
-    ws.newWeights = _newWeights;
-
-    require(
-      _endBlock > _startBlock,
-      "PWeightControlledSmartPool.updateWeightsGradually: End block must be after start block"
-    );
-
-    delete ws.startWeights;
-
-    for (uint256 i = 0; i < tokens.length; i++) {
-      // startWeights are current weights
-      ws.startWeights.push(s.bPool.getDenormalizedWeight(tokens[i]));
-    }
+    LibWeights.updateWeightsGradually(_newWeights, _startBlock, _endBlock);
   }
 
   function pokeWeights() external noReentry {
-    PAStorage.StorageStruct storage ws = PAStorage.load();
-    pbs storage s = lpbs();
-    require(block.number >= ws.startBlock, "ERR_CANT_POKE_YET");
-
-    // This allows for pokes after endBlock that get weights to endWeights
-    uint256 minBetweenEndBlockAndThisBlock;
-    if (block.number > ws.endBlock) {
-      minBetweenEndBlockAndThisBlock = ws.endBlock;
-    } else {
-      minBetweenEndBlockAndThisBlock = block.number;
-    }
-
-    uint256 blockPeriod = ws.endBlock.bsub(ws.startBlock);
-    uint256 weightDelta;
-    uint256 newWeight;
-    address[] memory tokens = s.bPool.getCurrentTokens();
-    for (uint256 i = 0; i < tokens.length; i++) {
-      if (ws.startWeights[i] >= ws.newWeights[i]) {
-        weightDelta = ws.startWeights[i].bsub(ws.newWeights[i]);
-        newWeight = ws.startWeights[i].bsub(
-          (minBetweenEndBlockAndThisBlock.bsub(ws.startBlock)).bmul(weightDelta.bdiv(blockPeriod))
-        );
-      } else {
-        weightDelta = ws.newWeights[i].bsub(ws.startWeights[i]);
-        newWeight = ws.startWeights[i].badd(
-          (minBetweenEndBlockAndThisBlock.bsub(ws.startBlock)).bmul(weightDelta.bdiv(blockPeriod))
-        );
-      }
-      s.bPool.rebind(tokens[i], s.bPool.getBalance(tokens[i]), newWeight);
-    }
+    LibWeights.pokeWeights();
   }
 
   function applyAddToken() external noReentry onlyController {
--- contracts/smart-pools/libraries/LibWeights.sol
+++ contracts/smart-pools/libraries/LibWeights.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.6.4;
 
 import { PBasicSmartPoolStorage as PBStorage } from "../storage/PBasicSmartPoolStorage.sol";
+import { PAdjustableSmartPoolStorage as PAStorage } from "../storage/PAdjustableSmartPoolStorage.sol";
+import { PCTokenStorage as PCStorage } from "../storage/PCTokenStorage.sol";
 import { LibConst as constants } from "./LibConst.sol";
 import "./LibPoolToken.sol";
-import { PCTokenStorage as PCStorage } from "../storage/PCTokenStorage.sol";
 import "../../Math.sol";
 
 library LibWeights {
@@ -67,6 +68,80 @@ library LibWeights {
 
             LibPoolToken._mint(msg.sender, poolShares);
         }
+  }
+
+  function updateWeightsGradually(
+    uint256[] calldata _newWeights,
+    uint256 _startBlock,
+    uint256 _endBlock
+  ) external {
+    PBStorage.StorageStruct storage s = PBStorage.load();
+    PAStorage.StorageStruct storage ws = PAStorage.load();
+
+    uint256 weightsSum = 0;
+    address[] memory tokens = s.bPool.getCurrentTokens();
+    // Check that endWeights are valid now to avoid reverting in a future pokeWeights call
+    for (uint256 i = 0; i < tokens.length; i++) {
+      require(_newWeights[i] <= constants.MAX_WEIGHT, "ERR_WEIGHT_ABOVE_MAX");
+      require(_newWeights[i] >= constants.MIN_WEIGHT, "ERR_WEIGHT_BELOW_MIN");
+      weightsSum = weightsSum.badd(_newWeights[i]);
+    }
+    require(weightsSum <= constants.MAX_TOTAL_WEIGHT, "ERR_MAX_TOTAL_WEIGHT");
+
+    if (block.number > _startBlock) {
+      // This means the weight update should start ASAP
+      ws.startBlock = block.number;
+    } else {
+        ws.startBlock = _startBlock;
+    }
+    ws.endBlock = _endBlock;
+    ws.newWeights = _newWeights;
+
+    require(
+      _endBlock > _startBlock,
+      "PWeightControlledSmartPool.updateWeightsGradually: End block must be after start block"
+    );
+
+    delete ws.startWeights;
+
+    for (uint256 i = 0; i < tokens.length; i++) {
+      // startWeights are current weights
+      ws.startWeights.push(s.bPool.getDenormalizedWeight(tokens[i]));
+    }
+  }
+
+  function pokeWeights() external {
+    PBStorage.StorageStruct storage s = PBStorage.load();
+    PAStorage.StorageStruct storage ws = PAStorage.load();
+
+    require(block.number >= ws.startBlock, "ERR_CANT_POKE_YET");
+
+    // This allows for pokes after endBlock that get weights to endWeights
+    uint256 minBetweenEndBlockAndThisBlock;
+    if (block.number > ws.endBlock) {
+      minBetweenEndBlockAndThisBlock = ws.endBlock;
+    } else {
+      minBetweenEndBlockAndThisBlock = block.number;
+    }
+
+    uint256 blockPeriod = ws.endBlock.bsub(ws.startBlock);
+    uint256 weightDelta;
+    uint256 newWeight;
+    address[] memory tokens = s.bPool.getCurrentTokens();
+    for (uint256 i = 0; i < tokens.length; i++) {
+      if (ws.startWeights[i] >= ws.newWeights[i]) {
+        weightDelta = ws.startWeights[i].bsub(ws.newWeights[i]);
+        newWeight = ws.startWeights[i].bsub(
+          (minBetweenEndBlockAndThisBlock.bsub(ws.startBlock)).bmul(weightDelta.bdiv(blockPeriod))
+        );
+      } else {
+        weightDelta = ws.newWeights[i].bsub(ws.startWeights[i]);
+        newWeight = ws.startWeights[i].badd(
+          (minBetweenEndBlockAndThisBlock.bsub(ws.startBlock)).bmul(weightDelta.bdiv(blockPeriod))
+        );
+      }
+      s.bPool.rebind(tokens[i], s.bPool.getBalance(tokens[i]), newWeight);
+    }
   }
 
 }
