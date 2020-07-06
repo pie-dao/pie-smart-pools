9a487e245bd011eb0975b415353702989e493b52
Mick de Graaf <mick@mickdegraaf.nl>
2020-07-06 17:26:47 +0200

    Refactored Ownable and ReentryProtection storage

--- contracts/ReentryProtection.sol
+++ contracts/ReentryProtection.sol
@@ -1,32 +1,16 @@
 pragma solidity 0.6.4;
 
-
-// TODO move this generic contract to a seperate repo with all generic smart contracts
+import {ReentryProtectionStorage as RPStorage} from "./storage/ReentryProtectionStorage.sol";
 
 contract ReentryProtection {
-  bytes32 public constant rpSlot = keccak256("ReentryProtection.storage.location");
-
-  // reentry protection storage
-  struct rps {
-    uint256 lockCounter;
-  }
 
   modifier noReentry {
     // Use counter to only write to storage once
-    lrps().lockCounter++;
-    uint256 lockValue = lrps().lockCounter;
+    RPStorage.StorageStruct storage s = RPStorage.load();
+    s.lockCounter++;
+    uint256 lockValue = s.lockCounter;
     _;
-    require(lockValue == lrps().lockCounter, "ReentryProtection.noReentry: reentry detected");
+    require(lockValue == s.lockCounter, "ReentryProtection.noReentry: reentry detected");
   }
 
-  /**
-        @notice Load reentry protection storage
-        @return s Pointer to the reentry protection storage struct
-    */
-  function lrps() internal pure returns (rps storage s) {
-    bytes32 loc = rpSlot;
-    assembly {
-      s_slot := loc
-    }
-  }
 }
