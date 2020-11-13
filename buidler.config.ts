54db01f53214861d94f290a73d7683df10c61f76
puercha <puercha@puerchas-MacBook-Air.local>
2020-11-13 17:38:40 +0800

    部署成功v3.0

--- buidler.config.ts
+++ buidler.config.ts
@@ -36,17 +36,14 @@ const RINKEBY_PRIVATE_KEY_SECONDARY = process.env.RINKEBY_PRIVATE_KEY_SECONDARY 
 const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY || "";
 const MAINNET_PRIVATE_KEY_SECONDARY = process.env.MAINNET_PRIVATE_KEY_SECONDARY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID || "";
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY || "";
 
 const PLACE_HOLDER_ADDRESS = "0x1000000000000000000000000000000000000001";
-
-
-const INFURA_PROJECT_ID = "a665f4eae78c4da3a2b606003f9eb31a";
 
 // 将私钥替换为您的Ropsten帐户私钥
 // 要从Metamask导出私钥，请打开Metamask并去 账户详情 > 导出私钥
 // 注意切勿将真实的以太币放入测试账户
-const ROPSTEN_PRIVATE_KEY = "eb5361010735f94c64a486737fd438b78a1c1171ee537bf466d005bcf023eb8a";
-
 
 interface ExtendedBuidlerConfig extends BuidlerConfig {
   [x:string]: any
